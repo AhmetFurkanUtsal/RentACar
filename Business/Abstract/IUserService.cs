b705b07c13ee77abcdcbd879e7c8fc4de77bdfdf
Ahmet Furkan Utsal <furkan.utsal@hacettepe.edu.tr>
2024-08-22 16:55:02 +0300

    User

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService 
+    {
+        Result Add( User users);
+        Result Delete ( User user );
+
+        Result Update ( User user );
+
+        IDataResult <List<User>> GetAll ();
+
+        IDataResult<List<User>> GetbyId ( int id );
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        public Result Add(User users)
+        {
+            return new Result(true);
+        }
+
+        public Result Delete(User user)
+        {
+            return new Result(true);
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(Messages.Listed);
+        }
+
+        public IDataResult<List<User>> GetbyId(int id)
+        {
+            return new SuccessDataResult<List<User>>();
+        }
+
+        public Result Update(User user)
+        {
+            return new Result(true);
+        }
+    }
+}
